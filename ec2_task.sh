679baa37da6a19e8a165e57552e9687a5c158fa2
anna <anna@ubuntu.myguest.virtualbox.org>
2023-05-04 21:33:41 +0300

    add elastic IP to instances on creation, stop the first two instances

--- ec2_task.sh
+++ ec2_task.sh
@@ -12,6 +12,11 @@ VPC_ID=$(aws ec2 describe-vpcs --query 'Vpcs[?IsDefault].VpcId' --output text)
 PUBLIC_SUBNET=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$VPC_ID Name=map-public-ip-on-launch,Values=true --query 'Subnets[0].SubnetId' --output text)
 
 #echo "Public subnet is: $PUBLIC_SUBNET"
+ELASTIC_IPS=()
+for i in {0..2}; do
+    ELASTIC_IP=$(aws ec2 allocate-address --domain vpc --query 'PublicIp' --output text)
+    ELASTIC_IPS+=($ELASTIC_IP)
+done
 
 IMAGE_ID=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-hvm-*-x86_64-gp2" "Name=state,Values=available" --query "Images[0].ImageId" --output text)
 
@@ -26,11 +31,31 @@ for i in {0..2}; do
 
     INSTANCE_NAME=${INSTANCE_NAMES[i]}
 
-    #INSTANCE_ID=$(aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE  --subnet-id $SUBNET_ID --query 'Instances[0].InstanceId' --output text)
     INSTANCE_ID=$(aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE --subnet-id $SUBNET_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" --query 'Instances[0].InstanceId' --output text)
     echo "Instance $INSTANCE_NAME created with ID: $INSTANCE_ID"
 
- 
-
-    #echo "Instance $i created: $INSTANCE_ID"
 done
+
+echo "Wait a minute for instaces to run"
+# Check if instances are running
+
+    for INSTANCE_NAME in "${INSTANCE_NAMES[@]}"; do
+        INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" --query 'Reservations[0].Instances[0].InstanceId' --output text)
+        echo "Wait a minute for instaces to run"
+        sleep 30
+        echo "Checking if instances are running"
+        INSTANCE_STATE=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].State.Name' --output text)
+        if [ "$INSTANCE_STATE" = "running" ]; then
+            echo "$INSTANCE_NAME is running"
+            #Add elastic IP 
+            aws ec2 associate-address --instance-id $INSTANCE_ID --public-ip ${ELASTIC_IPS[i]}
+                # Stop instances one and two
+            if [ "$INSTANCE_NAME" = "one" ] || [ "$INSTANCE_NAME" = "two" ]; then
+                aws ec2 stop-instances --instance-ids $INSTANCE_ID
+                echo "Instance $INSTANCE_NAME stopped"
+               
+            fi
+        else
+            sleep 30
+        fi
+    done
